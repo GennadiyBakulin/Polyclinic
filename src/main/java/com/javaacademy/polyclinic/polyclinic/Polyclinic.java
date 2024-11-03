b89005b12dbf18c7b43922d6a803761cdb68716a
Gennadiy Bakulin <g.bakulin@yandex.ru>
2024-11-03 11:55:24 +0700

    Создание класса Поликлиника

---
+++ src/main/java/com/javaacademy/polyclinic/polyclinic/Polyclinic.java
@@ -0,0 +1,31 @@
+package com.javaacademy.polyclinic.polyclinic;
+
+import com.javaacademy.polyclinic.boxoffice.BoxOffice;
+import com.javaacademy.polyclinic.doctor.Doctor;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class Polyclinic {
+
+  private final BoxOffice boxOffice;
+  private final Doctor dentist;
+  private final Doctor therapist;
+  private final Doctor juniorSurgeon;
+  private final Doctor seniorSurgeon;
+
+  public void treatWithDentist() {
+    boxOffice.acceptPayments(dentist.treatPerson());
+  }
+
+  public void treatWithTherapist() {
+    boxOffice.acceptPayments(therapist.treatPerson());
+  }
+
+  public void treatWithJuniorSurgeon() {
+    boxOffice.acceptPayments(juniorSurgeon.treatPerson());
+  }
+
+  public void treatWithSeniorSurgeon() {
+    boxOffice.acceptPayments(seniorSurgeon.treatPerson());
+  }
+}
